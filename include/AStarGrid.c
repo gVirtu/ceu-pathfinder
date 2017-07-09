b78ad8021ffd68069d44533edc09c6630f5d9c22
Giancarlo De Roberto <giandroberto@yahoo.com.br>
2017-07-09 02:47:02 -0300

    Add path chaser, support for dynamic map size, camera scroll

--- include/AStarGrid.c
+++ include/AStarGrid.c
@@ -1,29 +1,9 @@
-#define WORLD_W 25
-#define WORLD_H 19
+#define WORLD_MAX_W 100
+#define WORLD_MAX_H 100
 
-int world[WORLD_W * WORLD_H];
-/* = {
-    0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1,
-    0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0,
-    0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0,
-    0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0,
-    0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1,
-    0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1,
-    0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1,
-    0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1,
-    0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1,
-    0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0,
-    0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0,
-    0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
-    1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0,
-    1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1,
-    1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1,
-    0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1
-};
-*/
+int world[WORLD_MAX_W * WORLD_MAX_H];
+
+int world_w, world_h;
 
 typedef struct {
     int x;
@@ -34,23 +14,38 @@ void loadWorld(char* path) {
     printf("Loading level from file %s...\n", path); 
     FILE* file = fopen(path, "r");
     //printf("Opened file...\n"); 
-    char line[WORLD_W + 2];
+    char line[WORLD_MAX_W];
 
-    int x, y=0;
+    int x, y, buf=0;
     while (fgets(line, sizeof(line), file)) {
         //printf("Read %s...\n", line); 
-        for(x=0;x<WORLD_W;++x) {
-            world[x + y*WORLD_W] = (line[x] == '#');
+        for(x=0;line[x]!=0 && line[x]!='\n';++x) {
+            world[buf++] = (line[x] == '#');
         }
         ++y;
+        //printf("Stopped at %d\n", x);
     }
+    world_w = x;
+    world_h = y;
+    //printf("Got world %dx%d\n", world_w, world_h);
     //printf("Closed file...\n"); 
     fclose(file);
 }
 
+int clamp(int x, int xmin, int xmax) {
+    if (x>xmax) return xmax;
+    if (x<xmin) return xmin;
+    return x;
+}
+
+int sign(int x) {
+    if (x<0) return -1;
+    return 1;
+}
+
 int WorldAt(int x, int y) {
-    if (x >= 0 && x < WORLD_W && y >= 0 && y < WORLD_H) {
-        return world[y*WORLD_W+x];
+    if (x >= 0 && x < world_w && y >= 0 && y < world_h) {
+        return world[y*world_w+x];
     } else {
         return -1;
     }
