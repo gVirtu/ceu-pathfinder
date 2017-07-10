431d1851614571f6901dbf3fa475d24dab41c893
Giancarlo De Roberto <giandroberto@yahoo.com.br>
2017-07-10 00:51:18 -0300

    Add player, group blocks, add pseudoblocks, rework chasers, collisions

--- include/AStarGrid.c
+++ include/AStarGrid.c
@@ -1,7 +1,11 @@
 #define WORLD_MAX_W 100
 #define WORLD_MAX_H 100
+#define MAX_SPAWNERS 100
 
 int world[WORLD_MAX_W * WORLD_MAX_H];
+int zombies_x[MAX_SPAWNERS];
+int zombies_y[MAX_SPAWNERS];
+int zombies_count;
 
 int world_w, world_h;
 
@@ -16,11 +20,22 @@ void loadWorld(char* path) {
     //printf("Opened file...\n"); 
     char line[WORLD_MAX_W];
 
+    zombies_count = 0;
+
     int x, y, buf=0;
     while (fgets(line, sizeof(line), file)) {
         //printf("Read %s...\n", line); 
         for(x=0;line[x]!=0 && line[x]!='\n';++x) {
-            world[buf++] = (line[x] == '#');
+            if (line[x] == '#')
+                world[buf] = (line[x] == '#');
+            else if (line[x] == 'x')
+                world[buf] = 10;
+            else if (line[x] == '@') {
+                zombies_x[zombies_count] = x;
+                zombies_y[zombies_count] = y;
+                ++zombies_count;
+            }
+            ++buf;
         }
         ++y;
         //printf("Stopped at %d\n", x);
@@ -53,18 +68,37 @@ int WorldAt(int x, int y) {
 
 void PathNodeNeighbors(ASNeighborList neighbors, void *node, void *context) {
     PathNode *pathNode = (PathNode *)node;
+    char free_left = 0; char free_up = 0;
+    char free_right = 0; char free_down = 0;
 
     if (WorldAt(pathNode->x+1, pathNode->y) == 0) {
+        free_right = 1;
         ASNeighborListAdd(neighbors, &(PathNode){pathNode->x+1, pathNode->y}, 1);
     }
     if (WorldAt(pathNode->x-1, pathNode->y) == 0) {
+        free_left = 1;
         ASNeighborListAdd(neighbors, &(PathNode){pathNode->x-1, pathNode->y}, 1);
     }
     if (WorldAt(pathNode->x, pathNode->y+1) == 0) {
+        free_down = 1;
         ASNeighborListAdd(neighbors, &(PathNode){pathNode->x, pathNode->y+1}, 1);
     }
     if (WorldAt(pathNode->x, pathNode->y-1) == 0) {
+        free_up = 1;
         ASNeighborListAdd(neighbors, &(PathNode){pathNode->x, pathNode->y-1}, 1);
+    }
+
+    if (free_right && free_down && WorldAt(pathNode->x+1, pathNode->y+1) == 0) {
+        ASNeighborListAdd(neighbors, &(PathNode){pathNode->x+1, pathNode->y+1}, 1);
+    }
+    if (free_left && free_up && WorldAt(pathNode->x-1, pathNode->y-1) == 0) {
+        ASNeighborListAdd(neighbors, &(PathNode){pathNode->x-1, pathNode->y-1}, 1);
+    }
+    if (free_left && free_down && WorldAt(pathNode->x-1, pathNode->y+1) == 0) {
+        ASNeighborListAdd(neighbors, &(PathNode){pathNode->x-1, pathNode->y+1}, 1);
+    }
+    if (free_right && free_up && WorldAt(pathNode->x+1, pathNode->y-1) == 0) {
+        ASNeighborListAdd(neighbors, &(PathNode){pathNode->x+1, pathNode->y-1}, 1);
     }
 }
 
